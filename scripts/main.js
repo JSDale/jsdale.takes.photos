fe7f971f345e80126bbb8318411de3a08068c1f1
JSDale <57218102+JSDale@users.noreply.github.com>
2023-07-23 16:18:31 +0100

    added photos to main.js and added python script to print filenames

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,22 @@ function displayPhotos()
     var container = document.getElementById("photoContainer");
 
     var imageLocations = [
-        "photos/DSC_0081.jpg",
-        "photos/DSC_0091.jpg",
-        "photos/DSC_0125.jpg",
-        "photos/DSC_0128.jpg",
+        "DSC_0081.jpg",
+        "DSC_0091.jpg",
+        "DSC_0125.jpg",
+        "DSC_0128.jpg",
+        "IMG_0016.jpg",
+        "IMG_0036.jpg",
+        "IMG_0096.jpg",
+        "IMG_0237.jpg",
+        "IMG_9554.jpg",
+        "IMG_9555.jpg",
+        "IMG_9573.jpg",
+        "IMG_9594.jpg",
+        "IMG_9647.jpg",
+        "IMG_9857.jpg",
+        "IMG_9950.jpg",
+        "IMG_9955.jpg"
     ];
 
     container.innerHTML = "";
---
+++ scripts/python/PhotNameGetter.py
@@ -0,0 +1,9 @@
+import os
+
+directory = f"{os.getcwd()}\\photos"
+print(directory)
+
+files = os.listdir(directory)
+
+for file in files:
+    print(f"\"{file}\",")
