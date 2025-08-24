350d4317514b39810086e7cdd4499553e3b6f0b2
JSDALE <jdale994@googlemail.com>
2025-08-24 23:06:37 +0100

    now logs visits

--- website/app.js
+++ website/app.js
@@ -1,11 +1,19 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` || '.env.pub'});
 const express = require('express');
-const Path = require('path');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const
 {
     PORT
 } = process.env;
+const logPathPrefix = path.join(__dirname, "logs");
+if (!fs.existsSync(logPathPrefix))
+{
+    fs.mkdirSync(logPathPrefix, {recursive: true});
+}
+
+const logFile = path.join(logPathPrefix, "visits.log");
 
 var port = PORT;
 if (typeof port == 'undefined')
@@ -13,8 +21,19 @@ if (typeof port == 'undefined')
     port = 3000;
 }
 
-app.use(express.static(Path.join(__dirname, 'views')));
-app.use(express.static(Path.join(__dirname, 'public')));
+app.use((req, _, next) => {
+    const userAgent = req.get("User-Agent") || "Unknown";
+    const logEntry = `${new Date().toUTCString()} - IP:${req.ip} - METH:${req.method} URL:${req.originalUrl} - UA:${userAgent}\n`;
+    fs.appendFile(logFile, logEntry, (err) =>
+    {
+        if (err) console.error("Error logging visit: ", err);
+    });
+
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const mainController = require("./controllers/main");
 const gearController = require('./controllers/gear');
